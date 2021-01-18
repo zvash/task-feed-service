24e3b985d367998aa824e0d0309c96cc180161f2
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2021-01-18 09:49:46 +0330

    timeout increased

--- app/Services/AffiliateService.php
+++ app/Services/AffiliateService.php
@@ -206,7 +206,6 @@ class AffiliateService
                 return ['data' => $contents['data'], 'status' => 200];
             }
         } catch (GuzzleException $exception) {
-            dd($exception->getMessage());
             $data =  json_decode($exception->getResponse()->getBody()->getContents(), 1);
             return ['data' => $data['errors']['message'], 'status' => $exception->getCode()];
         }
